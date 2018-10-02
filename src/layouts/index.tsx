4bf6728115377c3f569e5ec3d8640b487868ccc3
Tae-geon Choi <contact@zfbe.com>
2018-10-02 18:06:29 +0900

    Fix index layout to build correctly

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
+import { Location } from "@reach/router";
 
 import "modern-normalize";
 import "../styles/normalize";
@@ -38,30 +39,37 @@ class IndexLayout extends React.Component {
     );
   }
 
-  private renderPage = (data: StaticQueryProps) => {
-    const isHome = window.location.pathname === "/";
-    return (
-      <LayoutRoot>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: "description",
-              content: data.site.siteMetadata.description,
-            },
-            {
-              name: "keywords",
-              content:
-                "Mesh Korea, 메쉬코리아, 블로그, 기술 블로그, tech blog, makers blog, PO, Product Owner, 기획, 디자인, 개발, QA, 서버, 프론트엔드, 안드로이드, 웹, AWS, 머신러닝",
-            },
-          ]}
-        />
-        <Header home={isHome} title={data.site.siteMetadata.title} />
-        <LayoutMain home={isHome}>{this.props.children}</LayoutMain>
-        <Footer />
-      </LayoutRoot>
-    );
-  };
+  private renderPage = (data: StaticQueryProps) => (
+    <Location>
+      {({ location }) => (
+        <LayoutRoot>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              {
+                name: "description",
+                content: data.site.siteMetadata.description,
+              },
+              {
+                name: "keywords",
+                content:
+                  "Mesh Korea, 메쉬코리아, 블로그, 기술 블로그, tech blog, makers blog, PO, Product Owner, 기획, 디자인, 개발, QA, 서버, 프론트엔드, 안드로이드, 웹, AWS, 머신러닝",
+              },
+            ]}
+          />
+
+          <Header
+            home={location.pathname === "/"}
+            title={data.site.siteMetadata.title}
+          />
+          <LayoutMain home={location.pathname === "/"}>
+            {this.props.children}
+          </LayoutMain>
+          <Footer />
+        </LayoutRoot>
+      )}
+    </Location>
+  );
 }
 
 export default IndexLayout;
