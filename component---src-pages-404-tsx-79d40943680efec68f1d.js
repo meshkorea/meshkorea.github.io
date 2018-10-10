482bd00e2532708948e1d0c9bfe7e0a5a68586bc
Tae-geon Choi <contact@zfbe.com>
2018-10-10 21:46:01 +0900

    Updates

---
+++ component---src-pages-404-tsx-79d40943680efec68f1d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{145:function(e,n,t){"use strict";t.r(n);var l=t(0),a=t(151),u=t(161),c=t(153),o=t(162);n.default=function(){return l.createElement(o.a,null,l.createElement(u.a,null,l.createElement(c.a,null,l.createElement("h1",null,"404: Page not found."),l.createElement("p",null,"You've hit the void. ",l.createElement(a.Link,{to:"/"},"Go back.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-79d40943680efec68f1d.js.map
